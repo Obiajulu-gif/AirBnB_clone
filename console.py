74b24da8cf6ae7f9a8713e3cdb89ac9ce324b051
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-02-11 16:46:52 +0100

    console 0.0.1 done

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
-    
+
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
@@ -33,6 +33,7 @@ class HBNBCommand(cmd.Cmd):
     def help_help(self):
         """Help message for help command"""
         print("Help command to display available commands")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
